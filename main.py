3f394f0103b17fc700041bf3fb7dc0a712764e87
Serg <s8.spivak@gmail.com>
2026-04-05 23:43:10 +0300

    Add user input and greeting functionality, update variable assignments

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ print(menu_dict)
 #У цьому словнику перераховані елементи облікового запису користувача Instagram
 user_dict = {'name': 'Wendy', 'active': False, 'followers': 2}
 print(user_dict)
+name = input("What's your name? ")
+print("Hello, " + name + "!")
+# скільки літер у слові "hello"?
+var1 = len("hello")
+var1 = 5
+# скільки пунктів у цьому списку слів привітання?
+var2 = ["hi", "hello", "ola"]
+var2 = 3
+menu_items = ['wrap', 'salad', 'sandwich', 'soup']
+print(menu_items[3])
