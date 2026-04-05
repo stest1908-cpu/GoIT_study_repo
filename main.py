06a35f0dd7b457339d4a126baa3613842faa7e97
Serg <s8.spivak@gmail.com>
2026-04-05 23:23:30 +0300

    Add user and menu dictionaries with print statements

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ print("Hello Git!")
 menu = ["wraps", "sandwiches", "soup", "salad"]
 for i in menu:
      print(i)
+empty_dict = {}
+print(empty_dict)
+#У цьому словнику перераховано загальну кількість пунктів меню
+menu_dict = {"sandwiches": 6, "wraps": 4, "soup": 1}
+print(menu_dict)
+#У цьому словнику перераховані елементи облікового запису користувача Instagram
+user_dict = {'name': 'Wendy', 'active': False, 'followers': 2}
+print(user_dict)
