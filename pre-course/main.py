a134b5c83a1db69ae93a1a322a557c2414596ecd
Serg <s8.spivak@gmail.com>
2026-04-08 23:11:15 +0300

    Fix: Update congratulatory message to include extra exclamation marks

--- pre-course/main.py
+++ pre-course/main.py
@@ -34,4 +34,4 @@ my_lucky_number = 7
 guess = int(input("Guess my lucky number! I think it is: "))
 while my_lucky_number != guess:
      guess = int(input("Oops! Not it. Try again: "))
-print("Congrats! You guessed it.")
+print("Congrats! You guessed it!!!")
