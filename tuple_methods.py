56ee85303ea2d1e3ccc97da960a6f2c50cd1d524
Serg <s8.spivak@gmail.com>
2026-04-08 23:07:52 +0300

    Add examples for Python tuple methods in tuple_methods.py

---
+++ tuple_methods.py
@@ -0,0 +1,14 @@
+#Python Tuple count() Method
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+
+x = thistuple.count(1)
+
+print(x)
+
+#Python Tuple index() Method
+
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+
+x = thistuple.index(8)
+
+print(x)
